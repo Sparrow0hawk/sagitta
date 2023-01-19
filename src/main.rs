f8cc456453ad0fdabc6705835fecd334eacffd87
Sparrow0hawk <alexjcoleman@gmail.com>
2023-01-19 16:46:45 +0000

    add searching for job ID
    quick and dirty implementation

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,25 @@ use std::io::BufRead;
 
 struct Args {
     file: String,
+    job_id: i32,
 }
 
 fn parse_args() -> Result<Args, lexopt::Error> {
     use lexopt::prelude::*;
 
     let mut file = None;
+    let mut job_id = 1;
     let mut parser = lexopt::Parser::from_env();
     while let Some(arg) = parser.next()? {
         match arg {
+            Short('j') | Long("job-id") => {
+                job_id = parser.value()?.parse()?;
+            }
             Value(val) if file.is_none() => {
                 file = Some(val.into_string()?);
             }
             Long("help") => {
-                println!("Usage: sagitta FILE");
+                println!("Usage: sagitta [-j|--job-id=JOB_ID] FILE");
                 std::process::exit(0);
             }
             _ => return Err(arg.unexpected()),
@@ -25,6 +30,7 @@ fn parse_args() -> Result<Args, lexopt::Error> {
 
     Ok(Args {
         file: file.ok_or("missing argument FILE")?,
+        job_id: job_id,
     })
 }
 
@@ -33,15 +39,18 @@ fn main() -> Result<(), lexopt::Error> {
 
     let open_file = read_file(&args.file).unwrap_or_else(|e| panic!("Error opening file: {e}"));
 
-    let _ = open_file
+    let line: _ = open_file
         .lines()
         .skip(4)
-        .map(|x| {
-            let x = x.unwrap();
-            println!("{:?}", JobInfo::new(x.split(":").collect::<Vec<&str>>()));
-        })
-        .collect::<()>();
+        .map(|l| l.unwrap())
+        .filter(|x| x.split(":").nth(5).unwrap().parse::<i32>().unwrap() == args.job_id)
+        .collect::<String>();
 
-    //println!("{:?}", parser);
+    if line.len() > 0 {
+        println!("{:?}", JobInfo::new(line.split(":").collect::<Vec<&str>>()));
+    } else {
+        println!("No job with ID {}", args.job_id);
+    }
+
     Ok(())
 }
